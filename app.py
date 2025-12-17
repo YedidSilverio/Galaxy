388277467e01774c11331cd5fd57196eca39b9f0
Yedid Silverio <yedidsilverio15@gmail.com>
2025-12-16 21:08:46 -0600

    Mis cambios antes de integrar FastQC

--- app.py
+++ app.py
@@ -323,4 +323,5 @@ def ejecutar_bowtie():
 
 # ---------------- Ejecutar servidor ---------------- #
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
